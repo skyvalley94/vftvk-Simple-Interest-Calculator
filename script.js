7ab9884fcf4232c16d3367306fc0348c9b689810
Ethan Pace <ethanpace94@gmail.com>
2022-02-26 21:27:29 -0500

    completed final project

--- script.js
+++ script.js
@@ -1,6 +1,25 @@
-function compute()
-{
-    p = document.getElementById("principal").value;
-    
+function compute() {
+  var principal = document.getElementById("principal").value;
+
+  if (principal <= 0) {
+    alert("Enter a positive number.");
+    document.getElementById("principal").focus();
+    return false;
+  }
+
+  var rate = document.getElementById("rate").value;
+  var years = document.getElementById("years").value
+  var interest = principal * years * rate / 100
+  var year = new Date().getFullYear() + parseInt(years);
+
+
+  document.getElementById("result").innerHTML = 'If you deposit <mark class="highlight">$' + principal + '</mark>\<br\>at an interest rate of <mark class="highlight">' + rate + '%</mark>,\<br\>You will receive an amount of <mark class="highlight">$' + interest + '</mark>\<br\>in the year <mark class="highlight">' + year + '</mark>.';
+
 }
-        
+
+function updateRate() {
+  var rateval = document.getElementById("rate").value;
+  document.getElementById("rate_val").innerText = rateval + '%';
+}
+
+document.getElementById("rate").addEventListener("input", updateRate);
